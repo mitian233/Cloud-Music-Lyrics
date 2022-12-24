d672d0cde459c22a12c62de3155ee022c74519fd
mitian233 <2304254165@qq.com>
2022-12-24 17:36:01 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,5 @@
-from flask import Flask,abort
-import requests
-import json
-import logging
+from flask import Flask,abort,send_file
+import io,requests,json,logging
 logging.basicConfig(level=logging.INFO)
 
 ua = {'User-Agent': 'Mozilla/5.0'}
@@ -13,10 +11,10 @@ def index():
    json_data = {'status': 'ok'}
    return json_data
 
-@app.route("/lrc/<string:musicId>.lrc")
+@app.route("/lrc/<string:musicId>")
 def getlrc(musicId):
    if (musicId == ''):
-      abort(500)
+      abort(404)
    else:
       try:
          raw = requests.get('https://cloudmusic.mikan.ac.cn/lyric?id=' + musicId, headers=ua)
@@ -32,3 +30,19 @@ def getlrc(musicId):
       else:
          logging.info('Successfully fetched lyric of '+musicId)
          return result
+
+@app.route("/lrc/<string:musicId>.lrc")
+def getlrcfile(musicId):
+   if (musicId == ''):
+      abort(500)
+   else:
+      try:
+         r = requests.get('https://cloudmusic.mikan.ac.cn/lrc/' + musicId, headers=ua)
+      finally:
+         if r.status_code == 200:
+            Buffer = io.BytesIO()
+            Buffer.write(r.content)
+            Buffer.seek(0)
+            return send_file(Buffer, attachment_filename=musicId + '.lrc', as_attachment=True)
+         else:
+            abort(r.status_code)
