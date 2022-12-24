214e793be3f52e14b598b23a4da448fe8bd715d2
mitian233 <2304254165@qq.com>
2022-12-24 17:19:26 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask,abort
 import requests
 import json
+import logging
+logging.basicConfig(level=logging.INFO)
 
 ua = {'User-Agent': 'Mozilla/5.0'}
 
@@ -8,15 +10,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return 'EOF'
+   json_data = {'status': 'ok'}
+   return json_data
 
 @app.route("/lrc/<string:musicId>.lrc")
 def getlrc(musicId):
    if (musicId == ''):
-      return 'EOF'
+      abort(500)
    else:
-      raw = requests.get('https://cloudmusic.mikan.fun/lyric?id=' + musicId, headers=ua)
-      rc = raw.content.decode()
-      jsonfile = json.loads(rc)
-      result = jsonfile['lrc']['lyric']
-      return result
+      try:
+         raw = requests.get('https://cloudmusic.mikan.ac.cn/lyric?id=' + musicId, headers=ua)
+         rc = raw.content.decode()
+         jsonfile = json.loads(rc)
+         result = jsonfile['lrc']['lyric']
+      except Exception as e:
+         logging.error(e)
+         abort(500)
+      except KeyError as e:
+         logging.error(e)
+         abort(500)
+      else:
+         logging.info('Successfully fetched lyric of '+musicId)
+         return result
